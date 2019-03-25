af2704e2b207650ab3e70df3ede141a881671508
SmallBoys <554645932@qq.com>
2019-03-25 17:07:38 +0800

    引入AGQL库,将数据库改为用环境变量获取

---
+++ src/main/java/xyz/steamfrog/configuration/AsyncGameQueryLibConfiguration.java
@@ -0,0 +1,31 @@
+package xyz.steamfrog.configuration;
+
+import com.ibasco.agql.protocols.valve.steam.webapi.SteamWebApiClient;
+import com.ibasco.agql.protocols.valve.steam.webapi.interfaces.SteamUser;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * ASQL库引入
+ *
+ * @author zhanghe
+ * @date 2019/3/25
+ */
+@Configuration
+public class AsyncGameQueryLibConfiguration {
+
+    @Value("${steamWebApiKey}")
+    private String steamWebApiKey;
+
+    @Bean
+    public SteamWebApiClient getSteamWebApiClient(){
+        return new SteamWebApiClient(steamWebApiKey);
+    }
+
+    @Bean
+    public SteamUser getSteamUser(){
+        return new SteamUser(getSteamWebApiClient());
+    }
+
+}
